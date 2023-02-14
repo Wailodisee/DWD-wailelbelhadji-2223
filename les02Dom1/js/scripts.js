74dbfa2acd4ae70ea7160c6ba912fc3607c08fcc
Wailodisee <elbelhadjiwail@gmail.com>
2023-02-14 23:45:32 +0100

    taak shcoenen

--- les02Dom1/js/scripts.js
+++ les02Dom1/js/scripts.js
@@ -7,9 +7,18 @@ const inpMail = frm.querySelector('#inpEmail');
 const inpMaat = frm.querySelector('#selMeasure');
 const msgMail = frm.querySelector('#msgEmail');
 const msgMaat = frm.querySelector('#msgMeasure');
+const price = document.querySelector('#lblMessage');
+const extra1 = document.querySelector('#accessoires');
+const extra2 = extra1.querySelector('.accessoire');
+const extra3 = extra2.querySelector('input');
+const extraprijs = extra3.name;
+const prijs1 = 12.29;
+const prijs2 = 8.89;
+const prijs3 = 18.70;
 
 
 frm.setAttribute('novalidate', 'novalidate');
+let basisprijs = 54.99;
 
 frm.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -31,15 +40,29 @@ frm.addEventListener('submit', function(e) {
         msgMaat.innerHTML = 'Vul een maat in !';
         numErrors++;
     }
+    if (extra1.checked == true) {
+        basisprijs = basisprijs + prijs1;
+    }
+    if (extra2.checked == true) {
+        basisprijs = basisprijs + prijs2;
+    }
+    if (extra3.checked == true) {
+        basisprijs = basisprijs + prijs3;
+    }
+
     if (numErrors == 0) {
-        frm.submit();
+        price.innerHTML = (`Je keuze: ${captShoe.innerHTML} maat ${inpMaat.value}(totaalprijs: € ${basisprijs})`);
     } 
 });
+
 
 thumbLinks.forEach(lnk => {
     lnk.addEventListener('click', function(e) {
        e.preventDefault ();
+       document.querySelector('#model .selected').classList.remove('selected');
+       lnk.classList.add('selected');
        imgShoe.src = lnk.href;
-       captShoe.innerHTML = lnk.innerHTML;
+       captShoe.innerHTML = lnk.textContent;
     });
 });
+
