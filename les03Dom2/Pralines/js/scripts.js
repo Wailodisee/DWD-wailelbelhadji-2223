6b60749084df59acd6bc4eb0a2d5ba3f8499995a
Wailodisee <elbelhadjiwail@gmail.com>
2023-03-14 22:38:12 +0100

    pralines

--- les03Dom2/Pralines/js/scripts.js
+++ les03Dom2/Pralines/js/scripts.js
@@ -1,7 +1,7 @@
 const images = document.querySelectorAll('img');
 const span1 = document.querySelector('#span1');
 const span2 = document.querySelector('#span2');
-const span3 = document.querySelector('#span2');
+const span3 = document.querySelector('#span3');
 
 images.forEach((img) => {
   img.addEventListener('click', () => {
