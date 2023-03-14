a49d15425cbf1ca723fbfbf7887ea9bb733b1b15
Wailodisee <elbelhadjiwail@gmail.com>
2023-03-14 22:31:31 +0100

    taak pralines

--- les03Dom2/Pralines/js/scripts.js
+++ les03Dom2/Pralines/js/scripts.js
@@ -1,12 +1,15 @@
 const images = document.querySelectorAll('img');
+const span1 = document.querySelector('#span1');
+const span2 = document.querySelector('#span2');
+const span3 = document.querySelector('#span2');
 
 images.forEach((img) => {
   img.addEventListener('click', () => {
     const alt = img.getAttribute('alt');
     const calorie = img.getAttribute('data-calorie');
     const beschrijving = img.getAttribute('data-beschrijving');
-    document.querySelector('#span1').innerHTML = alt;
-    document.querySelector('#span2').innerHTML = calorie;
-    document.querySelector('#span3').innerHTML = beschrijving;
+    span1.innerHTML = alt;
+    span2.innerHTML = calorie;
+    span3.innerHTML = beschrijving;
   });
 });
