6e33fcec505863eed5e4a617f12d0f2e3a3cd43e
Wailodisee <elbelhadjiwail@gmail.com>
2023-03-17 01:05:59 +0100

    verbetering

--- les03Dom2/FotoGallerijNavigatie/js/scripts.js
+++ les03Dom2/FotoGallerijNavigatie/js/scripts.js
@@ -9,24 +9,46 @@ function showImage(lnk) {
   document.querySelector('.thumbs .active').classList.remove('active');
   lnk.classList.add('active');
 }
-thumbLinks.forEach((lnk, index) => {
+thumbLinks.forEach((lnk, ix) => {
   lnk.addEventListener('click', function(e) {
     e.preventDefault();
-    currentFoto = index;
+    currentFoto = ix;
     showImage(lnk);
   });
 });
+
 btnNext.addEventListener('click', function() {
-   currentFoto++; 
+  currentFoto++;
   if (currentFoto >= thumbLinks.length) {
-   currentFoto = 0;
+    currentFoto = 0;
   }
   showImage(thumbLinks[currentFoto]);
 });
+
 btnPrev.addEventListener('click', function() {
-   currentFoto--;
+  currentFoto--;
   if (currentFoto < 0) {
-   currentFoto = thumbLinks.length - 1;
+    currentFoto = thumbLinks.length - 1;
   }
   showImage(thumbLinks[currentFoto]);
+});
+document.addEventListener('keydown', function(el) {
+  if (el.key == 'ArrowRight') {
+    el.preventDefault();
+    currentFoto = currentFoto < thumbLinks.length - 1 ? currentFoto + 1 : 0;
+    showImage(thumbLinks[currentFoto]);
+  }
+  else if (el.key == 'ArrowLeft') {
+    el.preventDefault();
+    currentFoto = currentFoto > 0 ? currentFoto - 1 : thumbLinks.length - 1;
+    showImage(thumbLinks[currentFoto]);
+  }
+  else if (el.ctrlKey && el.key.match(thumbLinks.index)) {
+    el.preventDefault();
+    const indexes = parseInt(el.key) - 1;
+    if (indexes >= 0 && indexes < thumbLinks.length) {
+      currentFoto = indexes;
+      showImage(thumbLinks[currentFoto]);
+    }
+  }
 });
--- les03Dom2/Pralines/js/scripts.js
+++ les03Dom2/Pralines/js/scripts.js
@@ -1,15 +1,17 @@
 const images = document.querySelectorAll('img');
-const span1 = document.querySelector('#span1');
-const span2 = document.querySelector('#span2');
-const span3 = document.querySelector('#span3');
+const spannaam = document.querySelector('#span1');
+const spankcal = document.querySelector('#span2');
+const spanbesch = document.querySelector('#span3');
 
-images.forEach((img) => {
-  img.addEventListener('click', () => {
-    const alt = img.getAttribute('alt');
-    const calorie = img.getAttribute('data-calorie');
-    const beschrijving = img.getAttribute('data-beschrijving');
-    span1.innerHTML = alt;
-    span2.innerHTML = calorie;
-    span3.innerHTML = beschrijving;
-  });
+images.forEach(element => {
+  element.addEventListener('click', function(e)
+   {
+        e.preventDefault();
+        images.forEach(uitbr => 
+          uitbr.classList.remove('uitbrijding'));
+        this.classList.add('uitbrijding');
+        spannaam.innerHTML = element.alt;
+        spankcal.innerHTML = element.dataset.calorie;
+        spanbesch.innerHTML = element.dataset.beschrijving;
+    });
 });
